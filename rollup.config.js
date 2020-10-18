94ae93bc873c5fad521c8bc45538f9b56fa9e841
Ghassen Rjab <rjab.ghassen@gmail.com>
2020-10-18 08:56:44 +0200

    Add explicit exports default to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import pkg from "./package.json";
 export default {
   input: "src/main.js",
   output: [
-    { file: pkg.main, format: "cjs" },
+    { file: pkg.main, format: "cjs", exports: "default" },
     { file: pkg.module, format: "es" },
     { file: pkg.browser, format: "umd", name: pkg.name },
   ],
