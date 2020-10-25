aef67a7c0dadad65950bb90748a9f0addfa4a8ab
Ghassen Rjab <rjab.ghassen@gmail.com>
2020-10-25 04:45:29 +0100

    Use Logical assignment operators (#9)

--- src/main.js
+++ src/main.js
@@ -5,10 +5,7 @@ export default (array, categories) => {
   return array.reduce((result, item) => {
     categories.forEach(({ name, filter }) => {
       if (filter(item)) {
-        if (!result[name]) {
-          result[name] = [];
-        }
-        result[name].push(item);
+        (result[name] ??= []).push(item);
       }
     });
     return result;
